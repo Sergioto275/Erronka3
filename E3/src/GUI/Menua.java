9ff3187a7e1109f48ad44f9f25ffb333fbae5820
Sergioto275 <115075327+Sergioto275@users.noreply.github.com>
2023-04-18 08:51:42 +0200

    Eraketa eta lehen commita

---
+++ E3/src/GUI/Menua.java
@@ -0,0 +1,39 @@
+package GUI;
+
+import java.awt.*;
+import javax.swing.*;
+import javax.swing.border.EmptyBorder;
+
+public class Menua extends JFrame {
+
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Menua frame = new Menua();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public Menua() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+
+		setContentPane(contentPane);
+	}
+
+}
---
+++ E3/src/GUI/Saioa.java
@@ -0,0 +1,63 @@
+package GUI;
+
+import java.awt.*;
+import javax.swing.*;
+import javax.swing.border.EmptyBorder;
+
+public class Saioa extends JDialog {
+
+	private final JPanel contentPanel = new JPanel();
+	private JTextField txtErabil;
+	private JPasswordField passwordField;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		try {
+			Saioa dialog = new Saioa();
+			dialog.setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
+			dialog.setVisible(true);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * Create the dialog.
+	 */
+	public Saioa() {
+		setBounds(100, 100, 431, 321);
+		getContentPane().setLayout(new BorderLayout());
+		contentPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
+		getContentPane().add(contentPanel, BorderLayout.CENTER);
+		contentPanel.setLayout(null);
+		{
+			txtErabil = new JTextField();
+			txtErabil.setBounds(100, 112, 249, 26);
+			contentPanel.add(txtErabil);
+			txtErabil.setColumns(10);
+		}
+		
+		JLabel lblErabiltzailea = new JLabel("Erabiltzailea :");
+		lblErabiltzailea.setBounds(29, 112, 74, 26);
+		contentPanel.add(lblErabiltzailea);
+		
+		JLabel lblPasahitza = new JLabel("Pasahitza :");
+		lblPasahitza.setBounds(39, 172, 64, 26);
+		contentPanel.add(lblPasahitza);
+		
+		JLabel lblNewLabel_1_1 = new JLabel("Saioa Ireki");
+		lblNewLabel_1_1.setFont(new Font("Comic Sans MS", Font.BOLD | Font.ITALIC, 35));
+		lblNewLabel_1_1.setBounds(113, 24, 203, 50);
+		contentPanel.add(lblNewLabel_1_1);
+		
+		passwordField = new JPasswordField();
+		passwordField.setBounds(100, 172, 249, 26);
+		contentPanel.add(passwordField);
+		
+		JButton btnNewButton = new JButton("Login");
+		btnNewButton.setBounds(165, 236, 89, 23);
+		contentPanel.add(btnNewButton);
+	}
+}
